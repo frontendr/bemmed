01fb2225b7b11ca421cf394e6bdd65ba999e48b3
Frontendr <info@frontendr.com>
2019-04-08 21:00:33 +0200

    fix: Removed the module from the definition file and changed default export

--- declarations/bemmed.d.ts
+++ declarations/bemmed.d.ts
@@ -1,38 +1,34 @@
 declare type elementPart = string | number;
 declare type modifierPart = string | number | Object;
 
-declare module "bemmed" {
-  export class BEM {
-    private b?: String;
-    private e?: String;
-    private m?: String;
+export default class BEM {
+  private b?: String;
+  private e?: String;
+  private m?: String;
 
-    constructor(block: string, element?: string, modifier?: string);
+  constructor(block: string, element?: string, modifier?: string);
 
-    static propType: (props: any, propName: string, componentName: string) => void;
+  static propType: (props: any, propName: string, componentName: string) => void;
 
-    public concat(...items: Array<BEM | BEMList | string>): BEMList;
+  public concat(...items: Array<BEM | BEMList | string>): BEMList;
 
-    public element(element: string, ...modifiers: modifierPart[]): BEM;
+  public element(element: string, ...modifiers: modifierPart[]): BEM;
 
-    public elem: typeof BEM.prototype.element;
+  public elem: typeof BEM.prototype.element;
 
-    public modifier(...modifiers: modifierPart[]): BEM | BEMList;
+  public modifier(...modifiers: modifierPart[]): BEM | BEMList;
 
-    public mod: typeof BEM.prototype.modifier;
+  public mod: typeof BEM.prototype.modifier;
 
-    public elements(...elements: elementPart[]): BEMList;
+  public elements(...elements: elementPart[]): BEMList;
 
-    public withElem(...elements: elementPart[]): BEMList;
+  public withElem(...elements: elementPart[]): BEMList;
 
-    public withMod(...modifiers: modifierPart[]): BEMList;
+  public withMod(...modifiers: modifierPart[]): BEMList;
 
-    public toString(): string;
-  }
-
-  export default BEM;
-
-  export const BEMType: typeof BEM.propType;
-
-  export class BEMList extends Array {}
+  public toString(): string;
 }
+
+export const BEMType: typeof BEM.propType;
+
+export class BEMList extends Array {}
