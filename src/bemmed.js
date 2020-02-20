d52397e548cb02cc3d368c28dfa0483ff575b293
Frontendr <info@frontendr.com>
2020-02-20 17:06:36 +0100

    feat(prop-types): Refactored `BEM.propType` into a `BEM.propTypes` object with multiple `propTypes` functions.
    - `BEM.propTypes.bem`: New propType to check for `BEM` instances.
    - `BEM.propTypes.className`: PropType to check if a prop is a valid className (including BEM objects). This was the original `BEM.propType` which is still present but deprecated.
    - `BEM.propTypes.element`: PropType to check if a prop can be used as a value for `BEM.element()`.
    - `BEM.propTypes.modifier`: PropType to check if a prop can be used as a value for `BEM.modifier()`.
    
    ## Deprecations
    - `BEM.propType` is deprecated in favor of `BEM.propTypes.className`.
    - The `BEMTypes` export is also deprecated. It wasn't documented so doubt anyone uses it anyway.

--- src/bemmed.js
+++ src/bemmed.js
@@ -176,6 +176,157 @@ Object.assign(Proto, {
 });
 
 /**
+ * Creates the BEM.propTypes object.
+ * @returns {{modifier: ((function(*, *=, *=): Error)|undefined), className: ((function(*, *, *): Error)|undefined), bem: ((function(*, *=, *=): Error)|undefined), element: ((function(*, *=, *=): Error)|undefined)}}
+ */
+function getPropTypes() {
+  /**
+   * Returns a prop type error message.
+   * @param {string} propName The prop name
+   * @param {string} componentName The component name
+   * @param {Array} expected The expected types
+   * @param {string} actual The type of the value
+   * @returns {Error}
+   */
+  function propTypeError(propName, componentName, expected, actual) {
+    return new Error(
+      `Invalid prop '${propName}' provided to ${componentName}. Expected ${expected.join(
+        ", "
+      )}. Got ${actual}.`
+    );
+  }
+
+  /**
+   * Checker function if the value is a BEM instance.
+   * @param {*} value The value to check.
+   * @param {string} type The type of the value.
+   * @param {string} propName Name of the prop.
+   * @param {string} componentName Name of the component
+   * @returns {Error|undefined}
+   */
+  function bemPropType(value, type, propName, componentName) {
+    if (type !== "object" || !(value instanceof BEM || value instanceof BEMList)) {
+      return propTypeError(propName, componentName, ["an instance of BEM"], type);
+    }
+  }
+
+  /**
+   * Checker function if the value is a valid BEM element.
+   * @param {*} value The value to check.
+   * @param {string} type The type of the value.
+   * @param {string} propName Name of the prop.
+   * @param {string} componentName Name of the component
+   * @returns {Error|undefined}
+   */
+  function elementPropType(value, type, propName, componentName) {
+    const expected = ["string", "number"];
+    if (!expected.includes(type)) {
+      return propTypeError(propName, componentName, expected, type);
+    }
+  }
+
+  /**
+   * Checker function if the value is a valid BEM modifier.
+   * @param {*} value The value to check.
+   * @param {string} type The type of the value.
+   * @param {string} propName Name of the prop.
+   * @param {string} componentName Name of the component
+   * @returns {Error|undefined}
+   */
+  function modifierPropType(value, type, propName, componentName) {
+    const expected = ["object", "string", "number"];
+    if (!expected.includes(type)) {
+      return propTypeError(propName, componentName, expected, type);
+    }
+  }
+
+  /**
+   * Checker function if the value is a valid className prop.
+   * @param {*} value The value to check.
+   * @param {string} type The type of the value.
+   * @param {string} propName Name of the prop.
+   * @param {string} componentName Name of the component
+   * @returns {Error|undefined}
+   */
+  function classNamePropType(value, type, propName, componentName) {
+    const expected = ["string", "number"];
+    if (
+      !(expected.includes(type) || value instanceof BEM || value instanceof BEMList)
+    ) {
+      return propTypeError(
+        propName,
+        componentName,
+        expected.concat("instance of BEM or an instance of BEMList"),
+        type
+      );
+    }
+  }
+
+  /**
+   * Factory function for a propType function which ensures no Error is returned for
+   * falsy values if the prop is not required.
+   * @param {boolean} isRequired
+   * @param {function} func
+   * @returns {function(*, *=, *=): Error|undefined}
+   */
+  function getPropTypeChecker(isRequired, func) {
+    /**
+     * The propType function. Handles falsy values based on isRequired and calls the
+     * given function if a value is given.
+     * @param {Object} props Props object.
+     * @param {string} propName Prop name.
+     * @param {string} componentName Component name.
+     * @return {Error|undefined} Returns an error if an invalid value is given.
+     */
+    return function propType(props, propName, componentName) {
+      const value = props[propName];
+      const type = typeof value;
+      if (type === "undefined" && !isRequired) {
+        // falsy value and not required
+        return;
+      }
+
+      return func(value, type, propName, componentName);
+    };
+  }
+
+  /**
+   * Returns a non-required custom PropType function with a required variant on it's
+   * isRequired property.
+   * @param {function} func The checker function
+   * @returns {function(*, *=, *=): Error|undefined}
+   */
+  function getPropType(func) {
+    const propType = getPropTypeChecker(false, func);
+    propType.isRequired = getPropTypeChecker(true, func);
+    return propType;
+  }
+
+  return {
+    /**
+     * PropType for a BEM instance.
+     * @type {function(*, *=, *=): Error|undefined}
+     */
+    bem: getPropType(bemPropType),
+    /**
+     * PropType for any valid className prop value (which includes BEM(list) instances).
+     * @type {function(*, *, *): Error|undefined}
+     */
+    className: getPropType(classNamePropType),
+    /**
+     * PropType for a valid BEM element value.
+     * @type {function(*, *=, *=): Error|undefined}
+     */
+    element: getPropType(elementPropType),
+    /**
+     * PropType for a valid BEM modifier value.
+     * @type {function(*, *=, *=): Error|undefined}
+     */
+    modifier: getPropType(modifierPropType)
+  };
+}
+
+/**
  * Object with options for the created BEM class.
  * @typedef {{elementSeparator: string, modifierSeparator: string}} BEMOptions
  */
@@ -202,26 +353,19 @@ export function setup(options = {}) {
   }
 
   /**
-   * A propType that can be used to check a CSS class that can be a BEM instance or just a
-   * plain string.
-   * @param {Object} props Props object.
-   * @param {string} propName Prop name.
-   * @param {string} componentName Component name.
-   * @return {Error|undefined} Returns an error if the property is invalid.
+   * Collection of BEM related propTypes.
+   * @type {{modifier: ((function(*, *=, *=): Error)|undefined), className: ((function(*, *, *): Error)|undefined), bem: ((function(*, *=, *=): Error)|undefined), element: ((function(*, *=, *=): Error)|undefined)}}
    */
-  BEM.propType = function propType(props, propName, componentName) {
-    const value = props[propName];
-    if (
-      ["string", "number"].includes(typeof value) ||
-      value instanceof BEM ||
-      value instanceof BEMList
-    ) {
-      return;
-    }
-    return new Error(
-      `Invalid prop '${propName}' provided to ${componentName}. Should be a string or String instance got ${typeof value}`
-    );
-  };
+  BEM.propTypes = getPropTypes();
+
+  /**
+   * Previous versions had a propType property which checked if a property was a valid
+   * className property. This property is now deprecated in favor of
+   * BEM.propTypes.className and will be removed in the next major release.
+   * @type {((function(*, *, *): Error)|undefined)}
+   * @deprecated
+   */
+  BEM.propType = BEM.propTypes.className;
 
   /**
    * Set the prototype to Proto extended with the `es` and `ms` properties.
@@ -244,8 +388,9 @@ export const BEM = setup();
 
 /**
  * Export it's propType as BEMType.
+ * @deprecated
  */
-export const BEMType = BEM.propType;
+export const BEMTypes = BEM.propType;
 
 /**
  * And make the BEM class the default export.
