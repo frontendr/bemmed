caba5618be5f22cfc24fc6ea0de9e6418e35106e
Frontendr <info@frontendr.com>
2020-03-26 09:42:32 +0100

    feat(tests): Import mocha's `describe()` and `it()`.

--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,5 @@
 const assert = require("assert");
+const {describe, it} = require("mocha");
 
 const {BEM, BEMList, setup} = require("../lib/bemmed");
 const DefaultExport = require("../lib/bemmed").default;
